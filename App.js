2dee16e0ea785de63a6667a359906531052a17d6
Rachel <williamsrachelada@gmail.com>
2020-05-29 13:03:31 -0700

    Imported home and login components, changed loadClient function declaration syntax, created setCurrentUserId function to set user id in state, added login component in render statement and passed client object and setCurrentUserId function down as props

--- App.js
+++ App.js
@@ -1,6 +1,16 @@
+
+// mongodb stitch sdk reference for setting up client connection with db
+//https://docs.mongodb.com/stitch-sdks/js-react-native/4/index.html
+
+// "context" docs for passing props through stack.screen
+//https://reactjs.org/docs/context.html
+
 import React from 'react'
 import { Button, StyleSheet, Text, View } from 'react-native';
 import { Stitch, AnonymousCredential } from 'mongodb-stitch-react-native-sdk';
+
+import Login from './components/Login';
+import Home from './components/Home';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -9,13 +19,29 @@ export default class App extends React.Component {
       currentUserId: undefined,
       client: undefined
     };
-    this._loadClient = this._loadClient.bind(this);
-    this._onPressLogin = this._onPressLogin.bind(this);
-    this._onPressLogout = this._onPressLogout.bind(this);
+    this.loadClient = this.loadClient.bind(this);
+    this.setCurrentUserId = this.setCurrentUserId.bind(this);
   }
 
   componentDidMount() {
-    this._loadClient();
+    console.log("component mounted")
+    this.loadClient();
+  }
+
+  setCurrentUserId(userId) {
+    this.setState({ currentUserId: userId })
+    console.log(`Successfully logged in as user ${userId}`);
+  }
+
+  loadClient() {
+    Stitch.initializeDefaultAppClient('waterly-qyngc').then(client => {
+      this.setState({ client });
+
+      if(client.auth.isLoggedIn) {
+        this.setState({ currentUserId: client.auth.user.id })
+      }
+    });
+    console.log("client loaded")
   }
 
   render() {
@@ -25,50 +51,12 @@ export default class App extends React.Component {
       loginStatus = `Currently logged in as ${this.state.currentUserId}!`
     }
 
-    loginButton = <Button
-                    onPress={this._onPressLogin}
-                    title="Login"/>
-
-    logoutButton = <Button
-                    onPress={this._onPressLogout}
-                    title="Logout"/>
-
     return (
       <View style={styles.container}>
-        <Text> {loginStatus} </Text>
-        {this.state.currentUserId !== undefined ? logoutButton : loginButton}
+        <Text style={styles.text}> {loginStatus} </Text>
+        <Login client={this.state.client} setCurrentUserId={this.setCurrentUserId}/>
       </View>
     );
-  }
-
-  _loadClient() {
-    Stitch.initializeDefaultAppClient('waterly-qyngc').then(client => {
-      this.setState({ client });
-
-      if(client.auth.isLoggedIn) {
-        this.setState({ currentUserId: client.auth.user.id })
-      }
-    });
-  }
-
-  _onPressLogin() {
-    this.state.client.auth.loginWithCredential(new AnonymousCredential()).then(user => {
-        console.log(`Successfully logged in as user ${user.id}`);
-        this.setState({ currentUserId: user.id })
-    }).catch(err => {
-        console.log(`Failed to log in anonymously: ${err}`);
-        this.setState({ currentUserId: undefined })
-    });
-  }
-
-  _onPressLogout() {
-    this.state.client.auth.logout().then(user => {
-        console.log(`Successfully logged out`);
-        this.setState({ currentUserId: undefined })
-    }).catch(err => {
-        console.log(`Failed to log out: ${err}`);
-        this.setState({ currentUserId: undefined })
-    });
   }
 }
 
@@ -78,5 +66,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    paddingTop: 50,
   },
 });
